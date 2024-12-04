6e43e775b057781942659f24ef8a5e5c6b6aa8ec
prasannatdl <144869278+prasannatdl@users.noreply.github.com>
2024-12-04 11:59:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucket1" {
-	bucket = "neeha-first-bucket"
+	bucket = "demo-first-bucket"
 }
 
 resource "aws_s3_bucket" "bucket2" {
-    
+        bucket = "demo-second-bucket"     
 }
 
 provider "aws" {
