ea66d8a18eec2fb793810ae34d3f7dfd507cae18
prasannatdl <144869278+prasannatdl@users.noreply.github.com>
2024-12-04 11:18:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,3 @@ resource "aws_s3_bucket" "bucket2" {
 provider "aws" {
     region = "us-east-1"
 }
-
-terraform {
-  backend "s3" {
-    bucket         = "neeha-s3bucket-0412"
-    key            = "neeha/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "neeha-0412-dblocks"
-    encrypt        = true
-  }
-}
